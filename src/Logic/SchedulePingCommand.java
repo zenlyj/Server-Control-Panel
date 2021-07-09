a6277eb2fdb8c3b4d6372fee1d1c2a4b4d7c611d
Zen Lee Yijie <iarezenly@gmail.com>
2021-07-09 14:13:23 +0800

    Refactor schedule ping code
    Shift ScheduleService declaration to SchedulePingCommand class

--- src/Logic/SchedulePingCommand.java
+++ src/Logic/SchedulePingCommand.java
@@ -1,22 +1,37 @@
 package Logic;
 
+import Model.App;
 import Model.Server;
+import javafx.application.Platform;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SchedulePingCommand extends Command {
-    private List<Server> servers;
+    private App app;
+    private List<Server> serversSnapshot;
 
-    public SchedulePingCommand(List<Server> servers) {
-        this.servers = servers;
+    public SchedulePingCommand(App app) {
+        this.app = app;
+        this.serversSnapshot = listDeepCopy(app.getServers());
     }
 
-    @Override
-    public void execute() {
+    private List<Server> listDeepCopy(List<Server> servers) {
+        List<Server> serversSnapshot = new ArrayList<>();
         for (Server server : servers) {
+            serversSnapshot.add(new Server(server));
+        }
+        return serversSnapshot;
+    }
+
+    private void pingServers() {
+        for (Server server : serversSnapshot) {
             boolean isOnline = false;
             try {
                 isOnline = InetAddress.getByName(server.getIpAddress()).isReachable(300);
@@ -29,5 +44,53 @@ public class SchedulePingCommand extends Command {
             }
             server.setStatus(isOnline);
         }
+    }
+
+    private void updateMainApp() {
+        Platform.runLater(() -> {
+            int ptr = 0;
+            for (int i = 0; i < serversSnapshot.size(); i++) {
+                Server curr = serversSnapshot.get(i);
+                boolean isEdited = app.isServerInEdit(i);
+                boolean isDeleted = app.isServerInDelete(curr);
+                if (isEdited) {
+                    // release edited server
+                    app.removeServerInEdit(i);
+                }
+                if (isDeleted) {
+                    // release deleted server
+                    app.removeServerInDelete(curr);
+                    continue;
+                }
+                if (!isEdited) {
+                    app.getServers().set(ptr, curr);
+                }
+                ptr++;
+            }
+        });
+    }
+
+    private void setSchedule() {
+        ScheduledService<Void> service = new ScheduledService<Void>() {
+            @Override
+            protected Task<Void> createTask() {
+                return new Task<Void>() {
+                    @Override
+                    protected Void call() throws Exception {
+                        System.out.println("Starting schedule...");
+                        pingServers();
+                        updateMainApp();
+                        return null;
+                    }
+                };
+            }
+        };
+        service.setPeriod(Duration.seconds(10));
+        service.start();
+    }
+
+    @Override
+    public void execute() {
+        setSchedule();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,22 @@
-import Logic.PSCommand;
 import Logic.SchedulePingCommand;
 import Model.App;
-import Model.Server;
 import UI.MainPage;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.concurrent.ScheduledService;
-import javafx.concurrent.Task;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main extends Application{
-
-    private void setSchedule(App app) {
-        ScheduledService<Void> service = new ScheduledService<Void>() {
-            @Override
-            protected Task<Void> createTask() {
-                return new Task<Void>() {
-                    @Override
-                    protected Void call() throws Exception {
-                        System.out.println("Starting schedule...");
-                        // deep copy of server list
-                        List<Server> servers = new ArrayList<>();
-                        for (Server server : app.getServers()) {
-                            servers.add(new Server(server));
-                        }
-
-                        SchedulePingCommand spCommand = new SchedulePingCommand(servers);
-                        spCommand.execute();
-
-                        // update status of servers
-                        Platform.runLater(() -> {
-                            int ptr = 0;
-                            for (int i = 0; i < servers.size(); i++) {
-                                Server curr = servers.get(i);
-                                boolean isEdited = app.isServerInEdit(i);
-                                boolean isDeleted = app.isServerInDelete(curr);
-                                if (isEdited) {
-                                    // release edited server
-                                    app.removeServerInEdit(i);
-                                }
-                                if (isDeleted) {
-                                    // release deleted server
-                                    app.removeServerInDelete(curr);
-                                    continue;
-                                }
-                                if (!isEdited) {
-                                    app.getServers().set(ptr, curr);
-                                }
-                                ptr++;
-                            }
-                        });
-                        return null;
-                    }
-                };
-            }
-        };
-        service.setPeriod(Duration.seconds(10));
-        service.start();
-    }
-
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        System.out.println(PSCommand.invokeCommand("sesh", PSCommand.removeIPCommand("ada", "old")));
         App app = new App();
         MainPage mainPage = new MainPage();
         primaryStage.setTitle("Server Manager");
         Scene primaryScene = mainPage.getMainPage(app);
         primaryStage.setScene(primaryScene);
         primaryStage.show();
-        setSchedule(app);
+        SchedulePingCommand schedulePing = new SchedulePingCommand(app);
+        schedulePing.execute();
     }
 
     public static void main(String[] args) {
